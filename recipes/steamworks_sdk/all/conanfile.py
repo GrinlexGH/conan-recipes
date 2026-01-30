026d6acdf5359395fba4a9a54819a3c0bc098826
Max <aaalllooollk@gmail.com>
2026-01-30 16:28:00 +0300

    Update steamworks SDK

--- recipes/steamworks_sdk/all/conanfile.py
+++ recipes/steamworks_sdk/all/conanfile.py
@@ -3,6 +3,7 @@ import os
 from conan import ConanFile
 from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, collect_libs
 from conan.tools.scm import Git
+from conan.errors import ConanInvalidConfiguration
 
 required_conan_version = ">=2.20"
 
@@ -19,6 +20,22 @@ class SteamworksSDKRecipe(ConanFile):
 
     def export_sources(self):
         export_conandata_patches(self)
+
+    def validate(self):
+        os = str(self.settings.os)
+        arch = str(self.settings.arch)
+
+        if os == "Macos":
+            return
+
+        supported = {
+            "Windows": ["x86", "x86_64"],
+            "Linux": ["x86", "x86_64", "armv8"],
+            "Android": ["armv8"],
+        }
+
+        if os not in supported or arch not in supported[os]:
+            raise ConanInvalidConfiguration(f"Unsupported configuration: {os} / {arch}")
 
     def source(self):
         src_data = self.conan_data["sources"][self.version]
@@ -52,7 +69,7 @@ class SteamworksSDKRecipe(ConanFile):
             if self.settings.arch == "x86_64":
                 self.cpp_info.bindirs.append(os.path.join("bin", "win64"))
                 self.cpp_info.libdirs.append(os.path.join("lib", "win64"))
-            else:
+            elif self.settings.arch == "x86":
                 self.cpp_info.bindirs.append("bin")
                 self.cpp_info.libdirs.append("lib")
                 self.cpp_info.bibdirs.append(os.path.join("bin", "win32"))
@@ -62,13 +79,20 @@ class SteamworksSDKRecipe(ConanFile):
             if self.settings.arch == "x86_64":
                 self.cpp_info.bindirs.append(os.path.join("bin", "linux64"))
                 self.cpp_info.libdirs.append(os.path.join("lib", "linux64"))
-            else:
+            elif self.settings.arch == "x86":
                 self.cpp_info.bindirs.append(os.path.join("bin", "linux32"))
                 self.cpp_info.libdirs.append(os.path.join("lib", "linux32"))
+            elif self.settings.arch == "armv8":
+                self.cpp_info.bindirs.append(os.path.join("bin", "linuxarm64"))
+                self.cpp_info.libdirs.append(os.path.join("lib", "linuxarm64"))
 
         elif self.settings.os == "Macos":
             self.cpp_info.bindirs.append(os.path.join("bin", "osx"))
             self.cpp_info.libdirs.append(os.path.join("lib", "osx"))
+
+        elif self.settings.os == "Android" and self.settings.arch == "armv8":
+            self.cpp_info.bindirs.append(os.path.join("bin", "androidarm64"))
+            self.cpp_info.libdirs.append(os.path.join("lib", "androidarm64"))
 
         component_api = self.cpp_info.components["SteamAPI"]
         component_api.set_property("cmake_target_name", "SteamworksSDK::SteamAPI")
@@ -76,10 +100,11 @@ class SteamworksSDKRecipe(ConanFile):
         component_api.bindirs = self.cpp_info.bindirs
         component_api.libdirs = self.cpp_info.libdirs
 
-        component_ticket = self.cpp_info.components["AppTicket"]
-        component_ticket.set_property("cmake_target_name", "SteamworksSDK::AppTicket")
-        component_ticket.libs = [next(x for x in collect_libs(self) if "sdkencryptedappticket" in x)]
-        component_ticket.bindirs = self.cpp_info.bindirs
-        component_ticket.libdirs = self.cpp_info.libdirs
-        component_ticket.set_property("nosoname", True)
-        component_ticket.requires = ["SteamAPI"]
+        if self.settings.os != "Android":
+            component_ticket = self.cpp_info.components["AppTicket"]
+            component_ticket.set_property("cmake_target_name", "SteamworksSDK::AppTicket")
+            component_ticket.libs = [next(x for x in collect_libs(self) if "sdkencryptedappticket" in x)]
+            component_ticket.bindirs = self.cpp_info.bindirs
+            component_ticket.libdirs = self.cpp_info.libdirs
+            component_ticket.set_property("nosoname", True)
+            component_ticket.requires = ["SteamAPI"]
