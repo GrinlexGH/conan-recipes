93bac3825f9b96c8c4f86011d6d621f0a22b51c0
Max <aaalllooollk@gmail.com>
2026-04-05 21:46:13 +0300

    Add android support for sdl

--- recipes/sdl/all/conanfile.py
+++ recipes/sdl/all/conanfile.py
@@ -90,6 +90,8 @@ class SDLRecipe(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
+        # Copy android project
+        copy(self, "android-project/*", self.source_folder, self.package_folder)
         copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
 
     def package_info(self):
