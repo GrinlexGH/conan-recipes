ba0c30e9bf27b49489037661d488631e63c35773
Max <aaalllooollk@gmail.com>
2026-05-02 22:00:52 +0300

    Add entt

---
+++ recipes/entt/all/conanfile.py
@@ -0,0 +1,46 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, get, copy
+
+required_conan_version = ">=2.20"
+
+class EnTTRecipe(ConanFile):
+    name = "entt"
+    package_type = "header-library"
+    implements = ["auto_header_only"]
+
+    license = "MIT"
+
+    settings = "os", "arch", "compiler", "build_type"
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], destination=self.source_folder, strip_root=True)
+        apply_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["ENTT_INSTALL"] = True
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "LICENSE*", self.source_folder, os.path.join(self.package_folder, "licenses"))
+
+    def package_info(self):
+        self.cpp_info.builddirs = [""]
+        self.cpp_info.set_property("cmake_find_mode", "none")
