e3ebb61448ad7a0cbabae0abab19928f41dcd715
Max <aaalllooollk@gmail.com>
2026-07-29 19:57:01 +0300

    change conan version

--- recipes/boost/all/get_libraries.py
+++ recipes/boost/all/get_libraries.py
@@ -1,5 +1,6 @@
 # Helper script to get dependencies yaml
 import glob
+
 for x in sorted(x for p in ("libs/numeric/*/CMakeLists.txt", "libs/*/CMakeLists.txt") for x in glob.iglob(p)):
     x = x[5:-15]
     x = x.replace("/", "_")
